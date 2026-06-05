a7ee4cec3acd23013fbb950344dea1d0c4187eea
Lars Eble <lars@bluebottle.gg>
2026-06-05 18:02:34 +0200

    fix: hide smite reaction timer on closing

--- src/components/SmiteReaction/SmiteReaction.vue
+++ src/components/SmiteReaction/SmiteReaction.vue
@@ -98,6 +98,7 @@ const ringGradient = computed(() => {
 
 onUnmounted(() => {
     unsub()
+    if (smite.value) smite.value = undefined
     if (smiteRevealTimer) clearTimeout(smiteRevealTimer)
 })
 
